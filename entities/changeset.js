4b61d4bc26d769c4112d4babc47f49a8f5636eaa
Christian Schwarz <christian.schwarz@refactory.at>
2013-10-31 16:00:47 +0100

    Added support for num_changes

--- entities/changeset.js
+++ entities/changeset.js
@@ -1,13 +1,14 @@
 module.exports = Changeset;
 
-function Changeset(id, createdAt, closedAt, open, user, userId, boundingBox) {
+function Changeset(id, createdAt, closedAt, changeCount, open, user, userId, boundingBox) {
     this.id = id;
     this.createdAt = createdAt;
     this.closedAt = closedAt;
+    this.changeCount = changeCount;
     this.open = open;
     this.user = user;
     this.userId = userId;
     this.boundingBox = boundingBox;
-    
+
 	this.tags = {};
 }
